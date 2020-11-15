d6c219265000a5bc6517f9c5aac8ab69ec6a8870
bk7987 <bk7987@gmail.com>
2020-11-15 17:36:35 -0500

    Worked on async connection to NATS from submittals service

--- submittals/src/main.py
+++ submittals/src/main.py
@@ -1,20 +1,31 @@
 import asyncio
 from fastapi import FastAPI
+from stan.aio.client import Client as STAN
 from .nats_client import run
+
 
 app = FastAPI()
 
 
 @app.on_event('startup')
 async def startup():
-    pass
-
-
-@app.get("/")
-def read_root():
-    return {"message": "this is the root route of submittals!"}
+    await connect_nats()
 
 
 @app.get("/submittals")
-def read_submittals():
+async def read_submittals():
     return {"message": "this is the submittals endpoint"}
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "this is the root route of submittals!"}
+
+
+async def connect_nats():
+    loop = asyncio.get_event_loop()
+    future = asyncio.Future[STAN](loop=loop)
+    run_task = run(loop, future)
+    sc = await asyncio.wait_for(future, None, loop=loop)
+    print("Connected to NATS: ", sc)
+    loop.create_task(run_task)
--- submittals/src/nats_client.py
+++ submittals/src/nats_client.py
@@ -3,18 +3,56 @@ from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
 
 
-async def run(loop):
+async def run(loop: asyncio.AbstractEventLoop, future_sc: asyncio.Future[STAN]):
     # Use borrowed connection for NATS then mount NATS Streaming
     # client on top.
     nc = NATS()
-    await nc.connect(io_loop=loop)
+    await nc.connect(io_loop=loop, servers=["http://nats-srv:4222"])
 
     # Start session with NATS Streaming cluster.
     sc = STAN()
-    await sc.connect("test-cluster", "client-123", nats=nc)
+    await sc.connect("opuscm", "submittals", nats=nc)
+    future_sc.set_result(sc)
+
+    # Synchronous Publisher, does not return until an ack
+    # has been received from NATS Streaming.
+    # await sc.publish("hi", b'hello')
+    # await sc.publish("hi", b'world')
+    # total_messages = 0
+    # future = asyncio.Future(loop=loop)
+
+    # async def cb(msg):
+    #     nonlocal future
+    #     nonlocal total_messages
+    #     print(f"Received a message (seq={msg.seq}): {msg.data}")
+    #     total_messages += 1
+    #     if total_messages >= 3:
+    #         future.set_result(None)
+
+    # Subscribe to get all messages since beginning.
+    future = asyncio.Future(loop=loop)
+    sub = await sc.subscribe("test", start_at='first', cb=build_callback(future))
+    await asyncio.wait_for(future, 5, loop=loop)
+    print("future complete test!")
+
+    # Stop receiving messages
+    await sub.unsubscribe()
 
     # Close NATS Streaming session
     await sc.close()
 
     # We are using a NATS borrowed connection so we need to close manually.
     await nc.close()
+
+
+def build_callback(future: asyncio.Future):
+    total_messages = 0
+
+    async def callback(msg):
+        nonlocal future
+        nonlocal total_messages
+        print(f"Received a message (seq={msg.seq}): {msg.data}")
+        total_messages += 1
+        if total_messages >= 3:
+            future.set_result(None)
+    return callback
